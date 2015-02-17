0682b9ad484b5c744bae90fee9cdf09529477022
Oliver Johnstone <oliver.johnstone@clock.co.uk>
2015-02-17 19:36:44 +0000

    Refactoring to make more modular

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var ZHL16 = require('./lib/algorithms/zh-l16')
+  , tissueValues =
+    { zhl16a: require('./lib/tissues/zh-l16a')
+    , zhl16b: require('./lib/tissues/zh-l16b')
+    , zhl17b: require('./lib/tissues/zh-l17b')
+    }
+
 module.exports =
-  { Dive: require('./lib/dive')
+  { ZHL16A: ZHL16.bind(null, tissueValues.zhl16a)
+  , ZHL16B: ZHL16.bind(null, tissueValues.zhl16b)
+  , ZHL17B: ZHL16.bind(null, tissueValues.zhl17b)
   }
