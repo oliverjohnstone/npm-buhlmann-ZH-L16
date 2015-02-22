ceee01502783ee1bbb42285fd7aed3b4465c9698
Oliver Johnstone <oliver.johnstone@clock.co.uk>
2015-02-22 15:07:57 +0000

    Exporting gas class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var ZHL16 = require('./lib/algorithms/zh-l16')
+  , Gas = require('./lib/gas')
   , tissueValues =
     { zhl16a: require('./lib/tissues/zh-l16a')
     , zhl16b: require('./lib/tissues/zh-l16b')
@@ -9,4 +10,5 @@ module.exports =
   { ZHL16A: ZHL16.bind(null, tissueValues.zhl16a)
   , ZHL16B: ZHL16.bind(null, tissueValues.zhl16b)
   , ZHL17B: ZHL16.bind(null, tissueValues.zhl17b)
+  , Gas: Gas
   }
